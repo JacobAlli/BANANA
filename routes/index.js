13bf58f5a517e41a73e66a5c4fe4e274ea383cd4
JacobAlli <Jacob@DESKTOP-GBJFCTP.localdomain>
2018-01-15 17:23:23 -0600

    playing around with create methods

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,12 @@ router.get('/', function(req, res, next) {
   db.User.findAll().then((users) => {
       res.render('index', {users: users})
   })
+});
 
-
+router.post('/adduser', function(req, res){
+	db.User.create({user_name: req.body.username, password: req.body.password}).then(() => {
+		res.redirect('/');
+	});
 });
 
 module.exports = router;
