191cb42c39e56fdf3403136dabf8516fe8d73ee9
JacobAlli <jallison42@gmail.com>
2018-01-18 17:30:45 -0600

    passport auth progress

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var express = require('express');
+
+
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var session = require('express-session');
+var validate = require('express-validator');
+var LocalStrategy = require('passport-local').Strategy;
+var flash = require('connect-flash');
+
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -22,6 +31,42 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//for passport
+app.use(session({secret: 'secret', resave: true, saveUninitialized: true}));
+
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+
+app.use(validate({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
 
 app.use('/', index);
 app.use('/users', users);
--- models/user.js
+++ models/user.js
@@ -1,22 +1,28 @@
+var bcrypt = require('bcryptjs');
+
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define('User', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    user_name: DataTypes.STRING,
-    password: DataTypes.STRING,
-    user_type: DataTypes.STRING,
-    address: DataTypes.STRING,
-    zipcode: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        User.hasMany(models.SalesOrder)
-      }
-    }
-  });
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      user_name: DataTypes.STRING,
+      password: DataTypes.STRING,
+      user_type: DataTypes.STRING,
+      address: DataTypes.STRING,
+      zipcode: DataTypes.STRING,
+      },
+  );
+
+  User.prototype.generateHash = function(password){
+      return bcrypt.hash(password, bcrypt.genSaltSync(8));
+  }
+
+  User.prototype.validPassword = function(password){
+      return bcrypt.compare(password, this.password);
+  }
+
   return User;
-};
+}
