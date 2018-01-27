9ba5d987f0c12d104865c23f22644b549aae992f
Renee Ernst <reneeernst@Renees-MacBook-Pro.local>
2018-01-27 03:38:01 -0600

    heroku

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
+var env       = process.env.JAWSDB_URL || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
@@ -29,6 +29,7 @@ Object.keys(db).forEach(modelName => {
     db[modelName].associate(db);
   }
 });
+
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
