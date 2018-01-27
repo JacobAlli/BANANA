36be9962462844c6acb46c9847adc77f7dd3d6da
Renee Ernst <reneeernst@Renees-MacBook-Pro.local>
2018-01-26 21:48:14 -0600

    close to the end

--- public/javascripts/fullpageSections.js
+++ public/javascripts/fullpageSections.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
           $('#fullpage').fullpage({
-            sectionsColor: ['#fff',  '#677077','#252839','#f2b632']
+            sectionsColor: ['#fff',  '#677077','#252839','#f2b632'],
+            scrollBar: true,
           });
+          AOS.init({
+          	duration: 1200});
         });
