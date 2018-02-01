1967a4167b11b64f34eab50250d330c208477fad
JacobAlli <jallison42@gmail.com>
2018-02-01 17:59:37 -0600

    mobile fix

--- public/javascripts/fullpageSections.js
+++ public/javascripts/fullpageSections.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
           $('#fullpage').fullpage({
             sectionsColor: ['#fff',  '#677077','#252839','#f2b632'],
-            scrollBar: true,
+            autoScrolling: false
           });
           AOS.init({
           	duration: 1200});
+          
         });
